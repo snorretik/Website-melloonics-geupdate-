acc82a2c17ba41ee8cc00f51b7c16333cf339eac
snorretik <niels.magendans@gmail.com>
2025-07-01 16:15:21 +0200

    making git repository ready for heroku...

--- hello.py
+++ hello.py
@@ -6,67 +6,67 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/index", methods=["GET"])
+@app.route("/index.html", methods=["GET"])
 def index2():
     return render_template("index.html")
 
-@app.route("/onDisplay", methods=["GET"])
+@app.route("/onDisplay.html", methods=["GET"])
 def onDisplay():
     return render_template("onDisplay.html")
 
-@app.route("/dionSchilderWerk", methods=["GET"])
+@app.route("/dionSchilderWerk.html", methods=["GET"])
 def dionSchilderWerk():
     return render_template("dionschilderwerk.html")
 
-@app.route("/sudokuProject", methods=["GET"])
+@app.route("/sudokuProject.html", methods=["GET"])
 def sudokuProject():
     return render_template("sudokuProject.html")
 
-@app.route("/eigenEinde", methods=["GET"])
+@app.route("/eigenEinde.html", methods=["GET"])
 def eigenEinde():
     return render_template("eigenEinde.html")
 
-@app.route("/goedeDoelen", methods=["GET"])
+@app.route("/goedeDoelen.html", methods=["GET"])
 def goedeDoelen():
     return render_template("goedeDoelen.html")
 
-@app.route("/overalIsGif", methods=["GET"])
+@app.route("/overalIsGif.html", methods=["GET"])
 def overalIsGif():
     return render_template("overalIsGif.html")
 
-@app.route("/blog1", methods=["GET"])
+@app.route("/blog1.html", methods=["GET"])
 def blog1():
     return render_template("blog1.html")
 
-@app.route("/blog2", methods=["GET"])
+@app.route("/blog2.html", methods=["GET"])
 def blog2():
     return render_template("blog2.html")
 
-@app.route("/blog3", methods=["GET"])
+@app.route("/blog3.html", methods=["GET"])
 def blog3():
     return render_template("blog3.html")
 
-@app.route("/blog4", methods=["GET"])
+@app.route("/blog4.html", methods=["GET"])
 def blog4():
     return render_template("blog4.html")
 
-@app.route("/blog5", methods=["GET"])
+@app.route("/blog5.html", methods=["GET"])
 def blog5():
     return render_template("blog5.html")
 
-@app.route("/blog6", methods=["GET"])
+@app.route("/blog6.html", methods=["GET"])
 def blog6():
     return render_template("blog6.html")
 
-@app.route("/blog7", methods=["GET"])
+@app.route("/blog7.html", methods=["GET"])
 def blog7():
     return render_template("blog7.html")
 
-@app.route("/blog8", methods=["GET"])
+@app.route("/blog8.html", methods=["GET"])
 def blog8():
     return render_template("blog8.html")
 
-@app.route("/overMij", methods=["GET"])
+@app.route("/overMij.html", methods=["GET"])
 def overMij():
     return render_template("overMij.html")
 
